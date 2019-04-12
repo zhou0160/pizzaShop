826160238d3421e3a41d84f8e798561e4c61db7a
Amber Zhou <zhou0160@algonquinlive.com>
2019-04-12 15:30:59 -0400

    add the routes in the app

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use(require('express-mongo-sanitize')())
 
 app.use('/auth', require('./routes/auth/index'))
 
+app.use(require('./middleware/auth'))
+app.use('/api/pizzas', require('./routes/pizzas'))
+app.use('/api/ingredients', require('./routes/ingredients'))
+app.use('/api/orders', require('./routes/orders'))
+
 app.use(require('./middleware/logErrors'))
 app.use(require('./middleware/errorHandler'))
 
