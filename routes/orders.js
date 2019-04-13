db6ab14ac3bd1956c4ea8014a288aeb0bdb8c739
Amber Zhou <zhou0160@algonquinlive.com>
2019-04-13 13:48:55 -0400

    added auth to order router and modified typos

--- routes/orders.js
+++ routes/orders.js
@@ -4,83 +4,164 @@ const authAdmin = require('../middleware/authorization')
 const express = require('express')
 const router = express.Router()
 const ResourceNotFoundError = require('../exceptions/ResourceNotFound')
+const User = require('../models/User')
 
 
 router.get('/', async (req, res, next) => {
-    try{
-        const orders = await Orders.find().populate('customer').populate('pizzas')
-        res.send({ data: orders })
-    } catch(err){
+    try {
+        let user = await User.findById(req.user._id)
+        let orders = await Orders.find().populate('customer pizzas')
+        if (!user.isStaff) {
+            orders = await orders.map(item => {
+                if(item.customer._id == req.user._id){
+                    return item
+                }
+            })
+        }
+        res.send({
+            data: orders
+        })
+    } catch (err) {
         next(err)
     }
 })
 
 router.get('/:id', async (req, res, next) => {
-    try{
-        const order = await Orders.findById(req.params.id).populate('customer').populate('pizzas')
-        if(!order) throw new ResourceNotFoundError('Resource not found')
-        res.send({ data: order })
-    } catch (err){
+    try {
+        
+        let order = await Orders.findById(req.params.id).populate('customer').populate('pizzas')
+        let user = await User.findById(req.user._id)
+        if (!user.isStaff) {
+            if (order.customer._id != req.user._id) {
+                res.send({
+                    errors: [{
+                        status: 'Forbidden',
+                        code: '403',
+                        title: 'Sorry, this is not your order.'
+                    }]
+                })
+            }
+        }
+        if (!order) throw new ResourceNotFoundError('Resource not found')
+        res.send({
+            data: order
+        })
+    } catch (err) {
         next(err)
     }
 })
 
-router.post('/', authAdmin, sanitizeBody, async (req, res, next) => {
-    try{
-        const newOrder = new Orders(req.sanitizedBody)
+router.post('/', sanitizeBody, async (req, res, next) => {
+    try {
+        let newOrder = new Orders(req.sanitizedBody)
+        let user = await User.findById(req.user._id)
+        if (!user.isStaff) {
+        newOrder.customer._id = req.user._id
+        }
         await newOrder.save()
-        res.send({ data: newOrder })
-    } catch(err){
+        res.send({
+            data: newOrder
+        })
+    } catch (err) {
         next(err)
     }
 })
 
-router.patch('/:id', authAdmin, sanitizeBody, async (req, res, next) =>{
-    try{
-        const {_id, ...otherAttributes} = req.sanitizedBody
+router.patch('/:id', sanitizeBody, async (req, res, next) => {
+    try {
+        let theOrder = await Orders.findById(req.params.id).populate('customer').populate('pizzas')
+        let user = await User.findById(req.user._id)
+        if (!user.isStaff) {
+            if (theOrder.customer._id != req.user._id) {
+                res.send({
+                    errors: [{
+                        status: 'Forbidden',
+                        code: '403',
+                        title: 'Sorry, this is not your order.'
+                    }]
+                })
+            }
+        }
+        const {
+            _id,
+            ...otherAttributes
+        } = req.sanitizedBody
         const order = await Orders.findByIdAndUpdate(
-            req.params.id,
-            {
-                _id: req.params.id, ...otherAttributes
-            },
-            {
+            req.params.id, {
+                _id: req.params.id,
+                ...otherAttributes
+            }, {
                 new: true,
                 runValidators: true
             }
         )
         if (!order) throw new ResourceNotFoundError('Resource not found')
-        res.send({data: order})
-    } catch (err){
+        res.send({
+            data: order
+        })
+    } catch (err) {
         next(err)
     }
 })
 
-router.put('/:id', authAdmin, sanitizeBody,async (req, res, next) => {
-    try{
-        const {_id, ...otherAttributes} = req.sanitizedBody
+router.put('/:id', sanitizeBody, async (req, res, next) => {
+    try {
+        let theOrder = await Orders.findById(req.params.id).populate('customer').populate('pizzas')
+        let user = await User.findById(req.user._id)
+        if (!user.isStaff) {
+            if (theOrder.customer._id != req.user._id) {
+                res.send({
+                    errors: [{
+                        status: 'Forbidden',
+                        code: '403',
+                        title: 'Sorry, this is not your order.'
+                    }]
+                })
+            }
+        }
+        const {
+            _id,
+            ...otherAttributes
+        } = req.sanitizedBody
         const order = await Orders.findByIdAndUpdate(
             req.params.id,
-            otherAttributes,
-            {
+            otherAttributes, {
                 new: true,
                 overwrite: true,
                 runValidators: true
             }
         )
         if (!order) throw new ResourceNotFoundError('Resource not found')
-        res.send({ data: order})
+        res.send({
+            data: order
+        })
     } catch (err) {
         next(err)
     }
 })
 
-router.delete('/:id', authAdmin, async (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
     try {
+        let theOrder = await Orders.findById(req.params.id).populate('customer').populate('pizzas')
+        let user = await User.findById(req.user._id)
+        if (!user.isStaff) {
+            if (theOrder.customer != req.user._id) {
+                res.send({
+                    errors: [{
+                        status: 'Forbidden',
+                        code: '403',
+                        title: 'Sorry, this is not your order.'
+                    }]
+                })
+            }
+        }
         const order = await Orders.findByIdAndRemove(req.params.id)
-        if (!order) throw new ErrResourceNotFoundErroror('Resource not found')
-        res.send({data: order})
+        if (!order) throw new ResourceNotFoundError('Resource not found')
+        res.send({
+            data: order
+        })
     } catch (err) {
-        next(err)        
+        next(err)
     }
 })
 
